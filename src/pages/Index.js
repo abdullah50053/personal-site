23bc382e09481c7c5b8819b744c8607a9afcace9
Abdullah Hasani <abdullah50053@gmail.com>
2023-01-08 21:20:07 -0600

    Add og image+description

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -7,6 +7,15 @@ const Index = () => (
   <Main
     description={"Abdullah Hasani's personal website."}
   >
+    <meta property="og:site_name" content="Abdullah Hasani" />
+    <meta property="og:title" content="Personal Site - Abdullah Hasani" />
+    <meta property="og:description" content="Abdullah Hasani's portfolio site" />
+    <meta property="og:url" content="https://abdullahhasani.com/" />
+    <meta property="og:image" content="https://asset.cloudinary.com/dx1x7j2ti/5dc8c4bf7a7679820ac512465c5be92c" />
+    <meta property="og:image:secure_url" content="https://asset.cloudinary.com/dx1x7j2ti/5dc8c4bf7a7679820ac512465c5be92c" />
+    <meta property="og:image:width" content="650" />
+    <meta property="og:image:height" content="650" />
+
     <article className="post" id="index">
       <header>
         <div className="title">
